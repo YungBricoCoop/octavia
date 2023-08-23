87271f25e7d24ae5fbecebbd078b00c230212808
Elwan <elwan.mayencourt@gmail.com>
2023-08-23 21:15:37 +0200

    ✨ Validate object from class util

--- src/Utils.php
+++ src/Utils.php
@@ -32,15 +32,85 @@ class Utils
 		$response = [
 			"data" => $data,
 		];
-		
+
 		// only include the error key if there is an error
 		if ($error) {
 			$response["error"] = $error;
 		}
-		
+
 		http_response_code($status);
 		header('Content-Type: application/json');
 		echo json_encode($response);
 		exit;
+	}
+
+	/**
+	 * Validate an object by using a class
+	 * @param mixed $object The object to validate
+	 * @param class $class The class to validate against
+	 * @return bool
+	 */
+	public static function validate($object, string $className)
+	{
+		$object = (object) $object;
+		$className = str_starts_with($className, "?") ? substr($className, 1) : $className;
+
+		if (!class_exists($className)) {
+			throw new \InvalidArgumentException("UNKNOWN_CLASS $className");
+			return false;
+		}
+
+		$reflection = new \ReflectionClass($className);
+		$properties = $reflection->getProperties();
+
+		// Parse all class properties
+		foreach ($properties as $property) {
+			$propertyName = $property->getName();
+			$type = $property->getType();
+
+			if (!property_exists($object, $propertyName)) {
+				return false;
+			}
+
+
+			if ($type->allowsNull() && $object->$propertyName === null) {
+				continue;
+			}
+
+			// Skip if no type is set
+			if (!$type) continue;
+
+			$expectedType = (string) $type;
+
+			if ($type->isBuiltin()) {
+				if ($expectedType === 'int' || $expectedType === '?int') {
+					$expectedType = 'integer';
+				}
+				if ($expectedType === 'bool' || $expectedType === '?bool') {
+					$expectedType = 'boolean';
+				}
+				if ($expectedType === 'float' || $expectedType === '?float') {
+					$expectedType = 'double';
+				}
+				if ($expectedType !== gettype($object->$propertyName)) {
+					print_r($expectedType);
+					print_r(gettype($object->$propertyName));
+					return false;
+				}
+				continue;
+			}
+
+			if (!class_exists($className)) {
+				throw new \InvalidArgumentException("UNKNOWN_CLASS $className");
+				return false;
+			}
+
+			if (!self::validate($object->$propertyName, $expectedType)) {
+				return false;
+			}
+
+			continue;
+		}
+		return true;
 	}
 }
