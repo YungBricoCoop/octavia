7f8514ab5e8e220278fa712149adcdc358e81c18
Lionel Ding <lionel.ding@hotmail.ch>
2023-10-13 17:11:07 +0200

    ✨ Finish GoogleOAuthHandler.php

--- src/Utils/GoogleOAuthHandler.php
+++ src/Utils/GoogleOAuthHandler.php
@@ -6,7 +6,8 @@ namespace ybc\octavia\Utils;
 use Google\Client as Google_Client;
 use Google\Service\Gmail as Google_Service_Gmail;
 
-class GoogleOAuthData {
+class GoogleOAuthData
+{
     public string $access_token;
     public string $refresh_token;
     public int $expires_in;
@@ -33,8 +34,8 @@ class GoogleOAuthData {
 
 class GoogleOAuthHandler
 {
-    private string $tokens_path = OCTAVIA_GOOGLE_OAUTH_DATA_PATH;
-    private string $oauth2_config_path = OCTAVIA_GOOGLE_OAUTH_CONFIG_PATH;
+    private string $data_path = OCTAVIA_GOOGLE_OAUTH_DATA_PATH;
+    private string $config_path = OCTAVIA_GOOGLE_OAUTH_CONFIG_PATH;
     private string $session_key = 'google_oauth_data';
     private $logger;
 
@@ -47,52 +48,101 @@ class GoogleOAuthHandler
         }
     }
 
-    public function get_tokens(): GoogleOAuthData
+    /**
+     * Gets the Google OAuth data from the cache.
+     * 
+     * @return GoogleOAuthData|null The data, or null if not found
+     */
+    private function get_cached_data(): ?GoogleOAuthData
     {
         if (isset($_SESSION[$this->session_key])) {
-            $data = $_SESSION[$this->session_key] as GoogleOAuthData;
-            if ($this->_is_access_token_expired($data->expires_at)) {
-                $data = $this->_refresh_access_token($data->refresh_token);
-            }
-            return $data;
+            return $_SESSION[$this->session_key];
         }
 
-        $data = $this->_read_from_file();
-        if ($this->_is_access_token_expired($data->expires_at)) {
-            $data = $this->_refresh_access_token($data->refresh_token);
-        }
-
-        $_SESSION[$this->session_key] = $data;
-        return $data;
+        return null;
     }
 
+    /**
+     * Gets the Google OAuth data from the cache or the tokens file.
+     * If the access token is expired, it will refresh it and save it to the cache and tokens file.
+     * 
+     * @return GoogleOAuthData The data
+     */
+    public function get_data(): GoogleOAuthData
+    {
+        $cached_data = $this->get_cached_data();
+
+        if (isset($cached_data)) {
+            if ($this->_is_access_token_expired($cached_data->expires_at)) {
+                return $this->_refresh_access_token($cached_data->refresh_token);
+            }
+
+            return $cached_data;
+        }
+
+        $file_data = $this->_read_from_file();
+        if ($this->_is_access_token_expired($file_data->expires_at)) {
+            return $this->_refresh_access_token($file_data->refresh_token);
+        }
+
+        return $file_data;
+    }
+
+    /** 
+     * Checks if the access token is expired
+     * 
+     * @param int $expires_at The timestamp of when the access token expires
+     * @return bool True if the access token is expired, false otherwise
+     */
     private function _is_access_token_expired(int $expires_at): bool
     {
         return time() > $expires_at;
     }
 
+    private function _save_data(GoogleOAuthData $data): void
+    {
+        $this->_write_to_file($data);
+        $_SESSION[$this->session_key] = $data;
+    }
+
+    /**
+     * Gets a new access token from Google OAuth using the refresh token, 
+     * and saves it to the tokens file and session (cache)
+     * 
+     * @param string $refresh_token
+     * @return GoogleOAuthData The new data
+     */
     private function _refresh_access_token(string $refresh_token): GoogleOAuthData
     {
         $this->logger->info("Refreshing Google OAuth access token");
+
+        // Handle the refreshing of the access token
         $client = new Google_Client();
-        $client->setAuthConfig($this->oauth2_config_path);
+        $client->setAuthConfig($this->config_path);
         $client->addScope(Google_Service_Gmail::MAIL_GOOGLE_COM);
         $client->setAccessType('offline');
         $client->fetchAccessTokenWithRefreshToken($refresh_token);
 
+        // Build the data structure
         $new_data = $this->_build_bean_from_google_client($client);
 
-        $this->_write_to_file($new_data);
-        $_SESSION[$this->session_key] = $new_data;
+        // Save the token information in file and session
+        $this->_save_data($new_data);
 
         return $new_data;
     }
 
+    /**
+     * Reads the tokens file and returns the data
+     * 
+     * @return GoogleOAuthData The data
+     * @throws \Exception If the file could not be read or the file is invalid
+     */
     private function _read_from_file(): GoogleOAuthData
     {
-        $content = file_get_contents($this->tokens_path);
+        $content = file_get_contents($this->data_path);
 
-        if (!$content) {
+        if ($content === FALSE) {
             throw new \Exception("Could not read tokens file");
         }
 
@@ -105,27 +155,48 @@ class GoogleOAuthHandler
         return new GoogleOAuthData($data['access_token'], $data['refresh_token'], $data['expires_in'], $data['expires_at']);
     }
 
+    /**
+     *  Writes the data to the tokens file
+     * 
+     * @param GoogleOAuthData $data The data to write
+     */
     private function _write_to_file(GoogleOAuthData $data): void
     {
-        file_put_contents($this->tokens_path, json_encode($data));
+        $result = file_put_contents($this->data_path, json_encode($data->to_array()));
+
+        if ($result === FALSE) {
+            throw new \Exception("Could not write to tokens file");
+        }
     }
 
+    /**
+     * Builds a GoogleOAuthData bean from a Google_Client
+     *  
+     * @param Google_Client $client The Google_Client to build from
+     * @return GoogleOAuthData The bean
+     */
     private function _build_bean_from_google_client(Google_Client $client): GoogleOAuthData
     {
         $new_token_config = $client->getAccessToken();
-        $access_token = $new_token_config['access_token'];
         $refresh_token = $client->getRefreshToken();
-        $expires_in = $access_token['expires_in'];
+        $access_token = $new_token_config['access_token'];
+        $expires_in = $new_token_config['expires_in'];
         $expires_at = time() + $expires_in;
 
         return new GoogleOAuthData($access_token, $refresh_token, $expires_in, $expires_at);
     }
 
+    /**
+     * Handles the initial Google OAuth prompt
+     * 
+     * @param string $redirect_uri The redirect URI to use
+     * @return bool True if the prompt was handled successfully, false otherwise
+     */
     public function handle_initial_prompt(string $redirect_uri): bool
     {
         $this->logger->info("Handling initial Google OAuth prompt");
         $client = new Google_Client();
-        $client->setAuthConfig($this->oauth2_config_path);
+        $client->setAuthConfig($this->config_path);
         $client->addScope(Google_Service_Gmail::MAIL_GOOGLE_COM);
         $client->setRedirectUri($redirect_uri); // Ensure this matches what you set in Google Cloud Console
         $client->setAccessType('offline'); // To get a refresh token
@@ -139,18 +210,17 @@ class GoogleOAuthHandler
         }
 
         // Authenticate and fetch the access token
-        $client->fetchAccessTokenWithAuthCode($_GET['code']);
+        $token = $client->fetchAccessTokenWithAuthCode($_GET['code']);
 
         // Check for errors
         if (isset($token['error']) || !isset($token['access_token'])) {
             return false;
         }
 
-        $data = $this->_build_bean_from_google_client($client);
+        $new_data = $this->_build_bean_from_google_client($client);
 
         // Save the token information for later usage
-        $this->_write_to_file($token);
-        $_SESSION[$this->session_key] = $data;
+        $this->_save_data($new_data);
 
         return true;
     }
